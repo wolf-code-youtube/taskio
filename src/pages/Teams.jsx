15f40c3238583b2d303bee497cff98d9076e0c1f
Wolf Code <sswolfgang69@gmail.com>
2024-11-10 18:58:58 +0530

    Update Teams.jsx

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Teams = () => {
     const [isDarkMode,setIsDarkMode] = useState(false);
@@ -24,19 +25,14 @@ const Teams = () => {
                 </div>
                 <div className='flex gap-5'>
                   <div>
-                  <a href="/">Home</a>
+                  <Link to="/">Home</Link>
                   </div>
                     <div>
-                      <a href="/documentation">
+                      <Link to="/documentation">
                         Documentation
-                        </a>
+                        </Link>
                     </div>
-                    <div>
-                        Contact Support
-                    </div>
-                    <div>
-                        About Us
-                    </div>
+                   
                 </div>
         </div>
         </div>
